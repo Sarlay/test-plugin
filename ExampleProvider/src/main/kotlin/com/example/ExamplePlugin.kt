f22a2cefac665fb6745ba2308c858c9b2136540d
Sarlay <60151189+Sarlay@users.noreply.github.com>
2022-08-21 12:07:21 +0200

    Should work now

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -1,13 +1,23 @@
 package com.example
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
 import com.lagradost.cloudstream3.plugins.Plugin
+
 import android.content.Context
 
 @CloudstreamPlugin
-class TestPlugin: Plugin() {
+
+class MesFilmsPlugin: Plugin() {
+
     override fun load(context: Context) {
+
         // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(ExampleProvider())
+
+        registerMainAPI(MesFilmsProvider())
+
     }
+
 }
+
+
